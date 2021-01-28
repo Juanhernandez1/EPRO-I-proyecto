e291c2c336433b5ee46b032ca977f1ea036ef6a4
Juanhernandez1 <2544692017@mail.utec.edu.sv>
2021-01-27 22:46:58 -0600

    eliminar todo

--- frontend/src/App/Core/Redux/Sagas/index.js
+++
@@ -1,7 +0,0 @@
-const { all, fork } = require('redux-saga/effects');
-const ObservandoCargos = require('./CargosSaga');
-const ObservandoEmpleados = require('./EmpleadoSaga');
-
-module.exports = function* rootSaga() {
-  yield all([fork(ObservandoEmpleados), fork(ObservandoCargos)]);
-};
