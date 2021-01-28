441c7f1b1d5e21b2eccabff729e87d4451615e1a
Juanhernandez1 <2544692017@mail.utec.edu.sv>
2021-01-28 03:16:25 -0600

    ✨ cambios en la configuracion

---
+++ backend/database/CitasDb.sql
@@ -0,0 +1,13 @@
+-- Database: CitasDB
+
+-- DROP DATABASE "CitasDB";
+
+CREATE DATABASE "CitasDB"
+    WITH 
+    OWNER = postgres
+    ENCODING = 'UTF8'
+    LC_COLLATE = 'es_SV.UTF-8'
+    LC_CTYPE = 'es_SV.UTF-8'
+    TABLESPACE = pg_default
+    CONNECTION LIMIT = -1;
+    
