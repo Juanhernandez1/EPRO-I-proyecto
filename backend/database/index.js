b98e030700d6c67438e437b973ffeb79d965aa21
Juanhernandez1 <2544692017@mail.utec.edu.sv>
2021-02-02 02:07:16 -0600

    🚨 test heroku

--- backend/database/index.js
+++ backend/database/index.js
@@ -3,9 +3,12 @@ import { config } from "dotenv";
 
 config();
 
-const CnDb = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS, {
-  host: process.env.DB_HOST,
-  dialect: process.env.DB_DIALECT
+const CnDb = new Sequelize(process.env.DATABASE_URL, {
+  dialect: "postgres",
+  protocol: "postgres",
+  dialectOptions: {
+    ssl: true
+  }
 });
 
 (async () => {
